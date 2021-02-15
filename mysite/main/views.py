0dca39452f04f3a6664d4134fa0f769a014a775c
Teslassian <dvoneschwege@gmail.com>
2021-02-15 15:03:46 +0200

    Completed part 7

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def homepage(request):
@@ -15,13 +16,20 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect("main:homepage")
+
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form": form})
 
     form = UserCreationForm
-    return render(request=request,
-                  template_name="main/register.html",
+    return render(request = request,
+                  template_name = "main/register.html",
                   context={"form": form})
